1cc41fd24613abee093d80383b39924d1566c067
BILYHO <627008265@qq.com>
2021-07-19 15:36:15 +0800

    删除测试组件

--- packages/vue/jsinfor-visc/nn-test/__tests__/nn-test.test.js
+++
@@ -1,5 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Element from '../src/nn-test.vue'
-
-describe('Jsi-NnTest', () => {
-})
--- packages/vue/jsinfor-visc/nn-test/index.js
+++
@@ -1,7 +0,0 @@
-import JsiNnTest from './src/nn-test.vue'
-
-JsiNnTest.install = Vue => {
-  Vue.component(JsiNnTest.name, JsiNnTest)
-}
-
-export default JsiNnTest
